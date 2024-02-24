be788e88f491a4a72407c2c272db5e864aafb329
Felipe Marques <felipe.tecnosuporte@gmail.com>
2024-02-24 20:39:04 -0300

    Páginas e componentes (UI) - Página: Cadastro de restaurante

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { Applayout } from './pages/_layouts/app'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
     {
@@ -13,7 +14,10 @@ export const router = createBrowserRouter([
     {
         path: '/',
         element: <Applayout />,
-        children: [{path: '/sign-in', element: <SignIn />}],
+        children: [
+            {path: '/sign-in', element: <SignIn />},
+            {path: '/sign-up', element: <SignUp />}
+        ],
     },
     
 ])
