a6ded2ef3783e6a90fffe5846f647177dc00015a
Felipe Marques <felipe.tecnosuporte@gmail.com>
2024-02-24 20:16:29 -0300

    Páginas e componentes (UI) - Notificações toast (sonner)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './global.css'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { Toaster } from 'sonner'
+
 export function App() {
   return (
     <HelmetProvider>
     <Helmet titleTemplate="%s | pizza.shop" />
-
+    <Toaster richColors />
     <RouterProvider router={router} />
   </HelmetProvider>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { toast } from 'sonner' 
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -13,11 +14,27 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { isSubmitting } 
+  } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          },
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
   return (
     <>
