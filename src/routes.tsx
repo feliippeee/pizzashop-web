c494916e9f48faf623cd8ffe996b07cbd073976d
Felipe Marques <felipe.tecnosuporte@gmail.com>
2024-12-13 00:54:44 -0300

    Página: Dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { Applayout } from './pages/_layouts/app'
 import { SignUp } from './pages/auth/sign-up'
