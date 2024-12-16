2ce90277a3f4feefa88ef153988f38e0e7248f3f
Felipe Marques <felipe.tecnosuporte@gmail.com>
2024-12-16 02:24:40 -0300

    Página 404 (Not Found)

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,6 +1,6 @@
 import { Header } from '@/components/header'
 import { Outlet } from 'react-router-dom'
-export function Applayout() {
+export function AppLayout() {
     return (
         <div className="flex min-h-screen flex-col antialiased">
             <Header />
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,41 @@
 import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
-import { SignIn } from './pages/auth/sign-in'
-import { Applayout } from './pages/_layouts/app'
-import { SignUp } from './pages/auth/sign-up'
 import { Orders } from './pages/app/orders/orders'
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
         path: '/',
-        element: <Applayout />,
-        children: [{path: '/', element: <Dashboard />}],
-    },
-    {
+        element: <Dashboard />,
+      },
+      {
         path: '/orders',
         element: <Orders />,
       },
-
-    {
-        path: '/',
-        element: <Applayout />,
-        children: [
-            {path: '/sign-in', element: <SignIn />},
-            {path: '/sign-up', element: <SignUp />}
-        ],
-    },
-    
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+    ],
+  },
 ])
