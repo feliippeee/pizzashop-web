b6bde2a916768320476ead22954b0c45c0fdbce2
Felipe Marques <felipe.tecnosuporte@gmail.com>
2024-12-31 16:36:07 -0300

    Configurando API client

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,8 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -12,7 +14,9 @@ export function App() {
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
+        <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
   </HelmetProvider>
   )
